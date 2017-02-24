de264280034a60b3f25716f666b151e258189e9b
stephenhou2 <stephenhou@163.com>
2017-02-24 11:23:40 +0800

    第一个swift正式练手项目，本人学习用

---
+++ SPWeibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SPWeibo
+//
+//  Created by 侯亮宏 on 17/2/24.
+//  Copyright © 2017年 侯亮宏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
